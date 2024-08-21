20abf90686b5ddaa926e5ef9b5b1f7c7ab7ea243
Mirlan0077 <mirlan.0077m@gmail.com>
2024-08-22 00:47:03 +0200

    добавил пути

---
+++ calculator/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'calculator'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('subtract/', views.subtract, name='subtract'),
+    path('multiply/', views.multiply, name='multiply'),
+    path('divide/', views.divide, name='divide'),
+]
--- hello/urls.py
+++ hello/urls.py
@@ -25,6 +25,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("webapp.urls")),
     path('accounts/', include("accounts.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-path('webapp/', include('webapp.urls')),
+    path('webapp/', include('webapp.urls')),
+    path('calculator/', include('calculator.urls')),
 
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
